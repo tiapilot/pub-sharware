9c38ad99a159b959e1fd77a0e81b262efd86c8f1
tiapilot <129731128+tiapilot@users.noreply.github.com>
2023-11-13 00:33:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,59 +2,62 @@ let stationData;
 let filteredData = null;
 
 function fetchData() {
-  return fetch('https://raw.githubusercontent.com/tiapilot/pub-sharware/main/data.json')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Errore nel caricamento dei dati: ${response.statusText}`);
-      }
-      return response.json();
-    });
+  return fetch(
+    "https://raw.githubusercontent.com/tiapilot/pub-sharware/main/data.json"
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(
+        `Errore nel caricamento dei dati: ${response.statusText}`
+      );
+    }
+    return response.json();
+  });
 }
 
 function renderTable(data) {
   stationData = data;
-  const container = document.getElementById('data-container');
-  container.innerHTML = '';
+  const container = document.getElementById("data-container");
+  container.innerHTML = "";
 
   stationData.forEach((station, index) => {
-    const row = document.createElement('tr');
+    const row = document.createElement("tr");
     row.innerHTML = `
       <td>${index + 1}</td>
-      <td>${station.customer}</td>
+      <td><a href="https://sharware.ecospazio.it${station['customer-url']}">${station.customer}</a></td>
+      <td><a href="https://sharware.ecospazio.it${station['sharing_station-url']}">${station.sharing_station}</a></td>
       <td>${station.status}</td>
-      <td>${station.bays_table.map(bay => `${bay.bay}</td>
-      <td>${bay['bay-status']}`).join('<br>')}</td>
-      <td>${station.vehicle_table.map(vehicle => `${vehicle.vehicle}</td>
-      <td>${vehicle['bay-status']}`).join('<br>')}</td>
+      <td>${station.bays_table.map((bay) => `<a href="https://sharware.ecospazio.it/${bay["bay-url"]}">${bay.bay}`).join("<br>")}</td>
+      <td>${station.bays_table.map((bay) => `${bay["bay-status"]}`).join("<br>")}</td>
+      <td>${station.vehicle_table.map((vehicle) => `${vehicle.vehicle}`).join("<br>")}</td>
+      <td>${station.vehicle_table.map((vehicle) => `${vehicle["vehicle-type"]}`).join("<br>")}</td>
     `;
     container.appendChild(row);
 
     if (index % 2 === 1) {
-      row.classList.add('even-row');
+      row.classList.add("even-row");
     }
   });
 
   updateFilterInfo();
 }
 
-
-
 function loadData() {
   fetchData()
-    .then(data => {
+    .then((data) => {
       renderTable(data);
       applyFilters();
     })
-    .catch(error => console.error(error));
+    .catch((error) => console.error(error));
 }
 
 function applyFilters() {
-  const searchCliente = getInputValue('searchCliente');
-  const searchStato = getInputValue('searchStato');
+  const searchCliente = getInputValue("searchCliente");
+  const searchStato = getInputValue("searchStato");
 
-  filteredData = stationData.filter(station =>
-    station.customer.toLowerCase().includes(searchCliente.toLowerCase()) &&
-    station.status.toLowerCase().includes(searchStato.toLowerCase())
+  filteredData = stationData.filter(
+    (station) =>
+      station.customer.toLowerCase().includes(searchCliente.toLowerCase()) &&
+      station.status.toLowerCase().includes(searchStato.toLowerCase())
   );
 
   renderTable(searchCliente || searchStato ? filteredData : stationData);
@@ -66,20 +69,25 @@ function getInputValue(inputId) {
 }
 
 function updateFilterInfo() {
-  const searchClienteValue = getInputValue('searchCliente');
-  const searchStatoValue = getInputValue('searchStato');
+  const searchClienteValue = getInputValue("searchCliente");
+  const searchStatoValue = getInputValue("searchStato");
 
-  const filterInfo = searchClienteValue ? `Cliente: ${searchClienteValue} ` : '';
-  const statoInfo = searchStatoValue ? `Stato: ${searchStatoValue} ` : '';
+  const filterInfo = searchClienteValue
+    ? `Cliente: ${searchClienteValue} `
+    : "";
+  const statoInfo = searchStatoValue ? `Stato: ${searchStatoValue} ` : "";
 
-  const filterButton = filterInfo || statoInfo ? `<button onclick="clearFilter()">x</button>` : '';
+  const filterButton =
+    filterInfo || statoInfo ? `<button onclick="clearFilter()">x</button>` : "";
 
-  document.getElementById('filter-info').innerHTML = `${filterInfo}${statoInfo}${filterButton}`;
+  document.getElementById(
+    "filter-info"
+  ).innerHTML = `${filterInfo}${statoInfo}${filterButton}`;
 }
 
 function clearFilter() {
-  setInputValue('searchCliente', '');
-  setInputValue('searchStato', '');
+  setInputValue("searchCliente", "");
+  setInputValue("searchStato", "");
 
   filteredData = null;
   loadData();
@@ -88,8 +96,8 @@ function clearFilter() {
 }
 
 function resetFilters() {
-  setInputValue('searchCliente', '');
-  setInputValue('searchStato', '');
+  setInputValue("searchCliente", "");
+  setInputValue("searchStato", "");
   filteredData = null;
   loadData();
 }
